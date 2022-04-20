5521b635659e303a8a41191f4a9a7ad2575c6fb2
Max Barr <mxbarr@gmail.com>
2022-04-20 15:25:43 -0700

    Python practice

---
+++ introduction/string_practice.py
@@ -0,0 +1,9 @@
+# Must convert int to string to concatenate 
+print("Hello " + str(42))
+
+total = 35
+user_value = "25"
+# Must convert string to int to add
+total = total + int(user_value)
+print(total)
+
