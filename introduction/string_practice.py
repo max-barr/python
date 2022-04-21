6ef6ee857d6eebba15ca8add179e57a6e67d2be5
Max Barr <mxbarr@gmail.com>
2022-04-21 12:33:40 -0700

    Python practice

--- introduction/string_practice.py
+++ introduction/string_practice.py
@@ -7,3 +7,18 @@ user_value = "25"
 total = total + int(user_value)
 print(total)
 
+# F Strings
+first_name = "Jimmy"
+last_name = "Chitwood"
+age = 19
+print(f"Hey, my name is {first_name} {last_name} and I'm {age} years old.")
+
+# String.format()
+print("Hey, my name is {} {} and I'm {} years old.".format(first_name, last_name, age))
+
+# % formatting
+print("My name is %s %s and I'm %d years old." % (first_name, last_name, age))
+
+# Split string method (default split is every space)
+print("Hello World!".split())
+print("Hello World!".split("o"))
