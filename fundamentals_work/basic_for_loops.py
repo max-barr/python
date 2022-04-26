e339e166036c2b6dae9481b5cc42290b401aba9c
Max Barr <mxbarr@gmail.com>
2022-04-26 11:34:00 -0700

    Practicing for loops

--- fundamentals_work/basic_for_loops.py
+++ fundamentals_work/basic_for_loops.py
@@ -13,3 +13,22 @@ for i in range(1,101):
     elif i%5 == 0:
         print("Coding")
     else: print(i)
+
+# Add odd integers from 0 to 500,000, and print the final sum.
+sum = 0
+for i in range(0,500001):
+    if i%2 == 1:
+        sum += i
+print(sum)
+
+# Print positive numbers starting at 2018, counting down by fours.
+for i in range(2018,0,-4):
+    print(i)
+
+# Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult. For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
+def flexible_counter(lowNum, highNum, mult):
+    for i in range(lowNum, highNum+1):
+        if i%mult == 0:
+            print(i)
+
+flexible_counter(2,9,3)
