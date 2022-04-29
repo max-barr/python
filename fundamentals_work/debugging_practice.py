e10f0e2601e2c6c4cd5cdd9cc4d85d35cd4fff5a
Max Barr <mxbarr@gmail.com>
2022-04-29 16:45:53 -0700

    Practicing debugging code

---
+++ fundamentals_work/debugging_practice.py
@@ -0,0 +1,18 @@
+# def multiply(num_list, num):
+#     print(num_list, num)
+#     for x in num_list:
+#         print(x)
+#         x *= num
+#         print(num_list)
+#     return num_list
+# a = [2,4,10,16]
+# b = multiply(a,5)
+# print(b)
+
+def multiply(num_list, num):
+    for x in range(len(num_list)):
+        num_list[x] *= num
+    return num_list
+a = [2,4,10,16]
+b = multiply(a,5)
+print(b)
