85beaf02deca69840adc6327c86ac329163cd656
Max Barr <mxbarr@gmail.com>
2022-05-03 15:45:34 -0700

    Practice chaining methods

--- oop/user2.py
+++ oop/user2.py
@@ -6,20 +6,21 @@ class User:
         self.account_balance = 0
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
     def make_withdrawal(self, amount):
         self.account_balance -= amount
+        return self
     def display_user_balance(self):
         print(f"Hi, {self.name}. Your balance is {self.account_balance}.")
+        return self
     def transfer_money(self, other_user, amount):
         self.account_balance -= amount
         other_user.account_balance += amount
+        return self
 
 Max = User("Max Williams", "max@gmail.com")
 Richard = User("Richard Brinkly", "richardb@yahoo.com")
 
-Max.make_deposit(150)
-Max.make_deposit(22)
-Max.transfer_money(Richard, 22)
-Max.display_user_balance()
+Max.make_deposit(150).make_deposit(22).transfer_money(Richard, 22).display_user_balance()
 Richard.display_user_balance()
 
