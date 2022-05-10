c62aef5eae2f7f9e128da5e429a772c5edb1ac91
Max Barr <mxbarr@gmail.com>
2022-05-10 15:28:05 -0700

    Practice associations between classes

--- extras/store_and_products2.py
+++ extras/store_and_products2.py
@@ -8,20 +8,18 @@ class Store:
         return self
 
     def sell_product(self, id):
-        for product in range(id, len(self.products) - 1):
-            temp = self.products[product]
-            self.products[product] = self.products[product + 1]
-            self.products[product + 1] = temp
-        self.products.pop()
-        return self
+        pass
 
     def inflation(self, percent_increase):
-        for product in range(0, len(self.products -1)):
+        for product in self.products:
             product.update_price(percent_increase, True)
         return self
 
     def set_clearance(self, category, percent_discount):
-        pass
+        for product in self.products:
+            if product.category == category:
+                product.price -= (product.price * percent_discount)
+        return self
 
     def print_products(self):
         for product in self.products:
