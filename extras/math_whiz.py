5618869cb542f4a3bc4fdeac5fe50a8e3d7840fb
Max Barr <mxbarr@gmail.com>
2022-05-11 11:25:39 -0700

    Practice with flexible functions

---
+++ extras/math_whiz.py
@@ -0,0 +1,12 @@
+class MathWhiz:
+    def __init__(self):
+        self.result = 0
+    def add(self, num, *nums):
+        pass
+    def subtract(self, num, *nums):
+        pass
+
+mw = MathWhiz()
+
+# x = mw.add(2).add(2,5,1).subtract(3,2).result
+# print(x)
