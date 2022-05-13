432be555495a56cb5b99d446f433ea7611e88ec2
Max Barr <mxbarr@gmail.com>
2022-05-13 15:53:53 -0700

    Practicing singly linked lists

--- extras/singly_linked_lists.py
+++ extras/singly_linked_lists.py
@@ -13,11 +13,11 @@ class SLList:
     # Traverse through the list
     def print_values(self):
         # A pointer to the list's first node
-        runner = self.head
+        current = self.head
         # While runner exists (runner != None)
-        while runner:
-            print(runner.value)
-            runner = runner.next
+        while current:
+            print(current.value)
+            current = current.next
         return self
 
     # Traverse through the list and adding a value to the end
@@ -28,12 +28,34 @@ class SLList:
             return self
         # Else:
         new_node = SLNode(val)
-        runner = self.head
-        while(runner.next != None):
-            runner = runner.next
-        runner.next = new_node
+        current = self.head
+        while(current.next != None):
+            current = current.next
+        current.next = new_node
         return self
 
+    # Remove the node at the front of the list
+    def remove_from_front(self):
+        if self.head != None:
+            self.head = self.head.next
+        return self
+
+    # Remove the node at the back of the list
+    def remove_from_back(self):
+        # Empty list
+        if self.head == None:
+            return self
+        # One node in the list
+        elif self.head.next == None:
+            self.head = None
+            return self
+        # At least 2 nodes in the list
+        else: 
+            current = self.head
+            while (current.next.next != None):
+                current = current.next
+            current.next = None
+        return self
 
 class SLNode:
     def __init__(self, val):
@@ -42,4 +64,4 @@ class SLNode:
 
 my_list = SLList()
 # my_list.add_to_front("are").add_to_front("Linked lists").add_to_back("fun!").print_values()
-my_list.add_to_front("B").add_to_front("A").print_values()
+my_list.add_to_front("B").add_to_front("A").add_to_back("C").add_to_back("Z").add_to_back("Y").remove_from_back().remove_from_back().print_values()
