051e1864449265eba77c9390487a2ffb39fd95cc
Max Barr <mxbarr@gmail.com>
2022-05-25 15:12:57 -0700

    Practicing Bubble Sort

--- extras/sorting_practice.py
+++ extras/sorting_practice.py
@@ -1,4 +1,14 @@
-arr = [2,4,6,8]
-arr[0], arr[1] = arr[1], arr[0]
-print(arr)
+# arr = [2,4,6,8]
+# arr[0], arr[1] = arr[1], arr[0]
+# print(arr)
 
+def bubbleSort(arr):
+    for m in range(len(arr)):
+        for i in range(len(arr)-1-m):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                print("swapped", arr[i], "and", arr[i+1])
+    return arr
+
+arr= [6,1,67,4,5,89,3,4,1]
+print(bubbleSort(arr))
