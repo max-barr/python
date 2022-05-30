bb88d693d517f5a0872da6d2f92e32eaefde43ba
Max Barr <mxbarr@gmail.com>
2022-05-30 11:45:35 -0700

    Practicing Selection Sort

--- extras/selection_sort.py
+++ extras/selection_sort.py
@@ -2,17 +2,18 @@
 
 def SelectionSort(arr):
     indexing_length = range(0, len(arr)-1)
-
+    
+    # Set the min value to i
     for i in indexing_length:
         min_value = i
-
+        # iterate through everything to the right of i. If j is less than min_value, j becomes the min_value.
         for j in range(i+1, len(arr)):
             if arr[j] < arr[min_value]:
                 min_value = j
-
+        # Swapping positions
         if min_value != i:
             arr[min_value], arr[i] = arr[i], arr[min_value]
 
     return arr
 
-print(SelectionSort([6,3,1,7,4,2]))
+print(SelectionSort([99,3,67,4,5,1,888,345]))
