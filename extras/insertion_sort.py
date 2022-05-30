231f8a41f9796dbbb6e2de113462afd6097a7336
Max Barr <mxbarr@gmail.com>
2022-05-30 12:18:38 -0700

    Practicing sorting algorithms

---
+++ extras/insertion_sort.py
@@ -0,0 +1,16 @@
+def InsertionSort(arr):
+
+    indexing_length = range(1, len(arr))
+
+    # The first item in the unsorted list is the first item in the sorted list. example: 1 | 4,5,6,74,
+    for i in indexing_length:
+        value_to_sort = arr[i]
+
+        # Is the value to the left higher than the value we are trying to sort? Also, i must be greater than zero because python allows negative indexing. We don't want to pull from the last value in the unsorted sublist.
+        while arr[i-1] > value_to_sort and i > 0:
+            arr[i], arr[i-1] = arr[i-1], arr[i]
+            i = i-1
+
+    return arr
+
+print(InsertionSort([2,3,4,5,5,5,33,4,6,756,43]))
--- extras/selection_sort.py
+++ extras/selection_sort.py
@@ -6,10 +6,12 @@ def SelectionSort(arr):
     # Set the min value to i
     for i in indexing_length:
         min_value = i
+        
         # iterate through everything to the right of i. If j is less than min_value, j becomes the min_value.
         for j in range(i+1, len(arr)):
             if arr[j] < arr[min_value]:
                 min_value = j
+        
         # Swapping positions
         if min_value != i:
             arr[min_value], arr[i] = arr[i], arr[min_value]
