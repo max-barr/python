3226a1e46aea9c3dff7354f4ed89872735f081a7
Max Barr <mxbarr@gmail.com>
2022-08-09 13:23:44 -0700

    Learning to unit test

---
+++ unit_testing/test_sum.py
@@ -0,0 +1,7 @@
+def test_sum():
+    # "Should be 6 is the assertion error message"
+    assert sum([1,2,3]) == 6, "Should be 6"
+
+if __name__ == "__main__":
+    test_sum()
+    print("Everything passed")
