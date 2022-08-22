4244b74743b8fcbb99d79ebffc007f1ebc4cb0bc
Max Barr <mxbarr@gmail.com>
2022-08-22 16:32:45 -0700

    Practice using assertions in python unit tests

--- unit_testing/test_assertion_practice.py
+++ unit_testing/test_assertion_practice.py
@@ -1,6 +1,45 @@
 import unittest
 
-class TestClass(unittest.TestCase):
+class Test():
+    pass
+
+class Test1():
+    pass
+
+class LearnAssertions(unittest.TestCase):
+
+    def test_sample(self):
+        a = 1
+        b = 1
+        self.assertEqual(a,b)
+
+    def test_sample2(self):
+        a = 1
+        b = 10
+        self.assertNotEqual(a,b)
+
+    def test_sample3(self):
+        a = 1
+        b = 1
+        self.assertIs(a,b)
+
+    def test_sample4(self):
+        a = 1
+        b = a
+        self.assertIs(a,b)
+
+    def test_sample5(self):
+        t1 = Test()
+        t2 = t1
+        self.assertIs(t1,t2)
+    
+    def test_sample6(self):
+        #These are different instances of Test()
+        t1 = Test()
+        t2 = Test()
+        self.assertIsNot(t1,t2)
+
+    
 
     
 
