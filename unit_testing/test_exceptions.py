c6c7b06483f585c9eef7f311844145de914502aa
Max Barr <mxbarr@gmail.com>
2022-08-23 15:52:51 -0700

    Learning unit tests with exceptions

--- unit_testing/test_exceptions.py
+++ unit_testing/test_exceptions.py
@@ -1,9 +1,19 @@
 import unittest
 
+def throw_ex(var):
+    if var == 100:
+        raise Exception("Not a valid number")
+    else:
+        return True
+
 class LearnUnitTesting(unittest.TestCase):
 
-    def sample_test(self):
-        pass
+    def test_sample(self):
+        self.assertEqual(throw_ex(10), True)
+
+    def test_sample2(self):
+        # First param is exception, 2nd is function (not calling the function), param the function will take is 3rd argument
+        self.assertRaises(Exception, throw_ex, 100)
 
 
 if __name__ == "__main__":
