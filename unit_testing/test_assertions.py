c5a9bb397caed685880060acc7a77ea24d385642
Max Barr <mxbarr@gmail.com>
2022-09-07 10:22:48 -0700

    Practice unit testing in Python

---
+++ unit_testing/test_assertions.py
@@ -0,0 +1,20 @@
+import unittest
+
+# Create class with camel case
+class TestingAssertions(unittest.TestCase):
+
+    def test_sample1():
+        pass
+
+    def test_sample2():
+        pass
+
+    def test_sample3():
+        pass
+
+
+
+
+# Gateway to the command line
+if __name__ == "__main__":
+    unittest.main()
