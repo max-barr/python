3a20ebdec5e9cbdfdf2775c288781be757fab0c9
Max Barr <mxbarr@gmail.com>
2022-12-19 16:35:16 -0800

    Practicing python functions

--- python_practice/fundamentals/basic_functions2.py
+++ python_practice/fundamentals/basic_functions2.py
@@ -23,14 +23,41 @@ print(print_and_return([1,2]))
 # First Plus Length - Create a function that accepts a list and returns the sum of the first value in the list plus the list's length.
 # Example: first_plus_length([1,2,3,4,5]) should return 6 (first value: 1 + length: 5)
 
+def first_plus_length(list):
+    return list[0] + len(list)
 
+print(first_plus_length([1,2,3,4,5,6,7,8]))
 
 
 # Values Greater than Second - Write a function that accepts a list and creates a new list containing only the values from the original list that are greater than its 2nd value. Print how many values this is and then return the new list. If the list has less than 2 elements, have the function return False
 # Example: values_greater_than_second([5,2,3,2,1,4]) should print 3 and return [5,3,4]
 # Example: values_greater_than_second([3]) should return False
 
+def greater_than_second(list):
+    new_list = []
+    count = 0
+    if len(list) < 2:
+        return False
+    else: 
+        for i in range(0, len(list)):
+            if list[i] > list[1]:
+                count += 1
+                new_list.append(list[i])
+        print("Count: " + str(count))
+        return new_list
+
+print(greater_than_second([2]))
+print(greater_than_second([5,2,3,2,1,4]))
 
 # This Length, That Value - Write a function that accepts two integers as parameters: size and value. The function should create and return a list whose length is equal to the given size, and whose values are all the given value.
 # Example: length_and_value(4,7) should return [7,7,7,7]
 # Example: length_and_value(6,2) should return [2,2,2,2,2,2]
+
+def length_and_value(size, value):
+    list = []
+    for i in range(0, size):
+        list.append(value)
+    return list
+
+print(length_and_value(4,7))
+print(length_and_value(6,2))
