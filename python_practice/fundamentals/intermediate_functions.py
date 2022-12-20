2c0365cd23b409dee6b0c8cbe212722518838bda
Max Barr <mxbarr@gmail.com>
2022-12-20 11:16:45 -0800

    Practicing intermediate python functions

--- python_practice/fundamentals/intermediate_functions.py
+++ python_practice/fundamentals/intermediate_functions.py
@@ -12,10 +12,20 @@ sports_directory = {
 z = [ {'x': 10, 'y': 20} ]
 
 # Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
+x[1][0] = 15
+print(x)
 
 # Change the last_name of the first student from 'Jordan' to 'Bryant'
+students[0]["last_name"] = "Bryant"
+print(students)
+
 # In the sports_directory, change 'Messi' to 'Andres'
+sports_directory['soccer'][0] = "Andres"
+print(sports_directory)
+
 # Change the value 20 in z to 30
+z[0]['y'] = 30
+print(z)
 
 # -------------------------------------------------------
 
