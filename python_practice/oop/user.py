6a647b6f04f946e32d03fad23d0303c98e801244
Max Barr <mxbarr@gmail.com>
2022-12-22 15:10:13 -0800

    Practice with classes, attributes, methods

--- python_practice/oop/user.py
+++ python_practice/oop/user.py
@@ -12,9 +12,24 @@ class User:
         self.account_balance += amount
         print(f"{self.name}'s account balance is now {self.account_balance} dollars.")
 
+    def make_withdrawal(self, amount):
+        self.account_balance -= amount
+        print(f"{self.name}'s account balance is now {self.account_balance} dollars.")
+
+    def display_user_balance(self):
+        print(f"{self.name}'s account balance is currently {self.account_balance} dollars.")
+
+    def transfer_money(self, other_user, amount):
+        self.account_balance -= amount
+        other_user.account_balance += amount
+        print(f"{other_user.name}'s account balance is now {other_user.account_balance} dollars.")
+        print(f"{self.name}'s account balance is now {self.account_balance} dollars.")
+
 # Create new instance
 max = User("Max", "max@yahoo.com")
+jimmy = User("Jimmy", "jimmy@hotmail.com")
 
 # Call instance methods
 max.greeting()
 max.make_deposit(500)
+max.transfer_money(jimmy, 300)
