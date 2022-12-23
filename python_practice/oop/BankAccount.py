d8b4b11bbdd40b55e339217a3871f06d57bf07fd
Max Barr <mxbarr@gmail.com>
2022-12-23 14:48:38 -0800

    Practice writing classes, methods

---
+++ python_practice/oop/BankAccount.py
@@ -0,0 +1,39 @@
+class BankAccount:
+    accounts = []
+    def __init__(self, name, int_rate, balance=0):
+        self.name = name
+        self.int_rate = int_rate
+        self.balance = balance
+        BankAccount.accounts.append(self)
+    # Class methods
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Deposit successful. Balance is now {self.balance}.")
+        return self
+    def withdraw(self, amount):
+        self.balance -= amount
+        print(f"Withdrawal successful. Balance is now {self.balance}.")
+        return self
+    def display_account_info(self):
+        print(f"{self.name} balance is currently {self.balance}")
+        return self
+    def yield_interest(self):
+        if self.balance > 0:
+            self.balance += self.balance * self.int_rate
+        else:
+            print("Insufficient funds.")
+        return self
+    
+    # Class method
+    @classmethod
+    def print_all_accounts(cls):
+        for account in cls.accounts:
+            account.display_account_info()
+
+checking = BankAccount("Checking", 0.01)
+savings = BankAccount("Savings", 0.02)
+
+checking.deposit(200).deposit(150).deposit(300).withdraw(100).yield_interest().display_account_info()
+savings.deposit(1000).deposit(2000).withdraw(150).withdraw(200).withdraw(150).withdraw(300).yield_interest().display_account_info()
+
+BankAccount.print_all_accounts()
--- python_practice/oop/user.py
+++ python_practice/oop/user.py
@@ -7,23 +7,28 @@ class User:
     # instance methods
     def greeting(self):
         print(f"Hello, my name is {self.name}!")
+        return self
 
     def make_deposit(self, amount):
         self.account_balance += amount
         print(f"{self.name}'s account balance is now {self.account_balance} dollars.")
+        return self
 
     def make_withdrawal(self, amount):
         self.account_balance -= amount
         print(f"{self.name}'s account balance is now {self.account_balance} dollars.")
+        return self
 
     def display_user_balance(self):
         print(f"{self.name}'s account balance is currently {self.account_balance} dollars.")
+        return self
 
     def transfer_money(self, other_user, amount):
         self.account_balance -= amount
         other_user.account_balance += amount
         print(f"{other_user.name}'s account balance is now {other_user.account_balance} dollars.")
         print(f"{self.name}'s account balance is now {self.account_balance} dollars.")
+        return self
 
 # Create new instance
 max = User("Max", "max@yahoo.com")
@@ -33,3 +38,6 @@ jimmy = User("Jimmy", "jimmy@hotmail.com")
 max.greeting()
 max.make_deposit(500)
 max.transfer_money(jimmy, 300)
+
+# Chain methods (returning self required)
+max.make_deposit(150).display_user_balance().make_withdrawal(75).transfer_money(jimmy, 20)
