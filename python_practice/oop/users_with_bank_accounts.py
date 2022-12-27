0667b775f011fd26bb9a93a9e05f6bf53a26b57c
Max Barr <mxbarr@gmail.com>
2022-12-26 16:12:45 -0800

    Practice associating classes

--- python_practice/oop/users_with_bank_accounts.py
+++ python_practice/oop/users_with_bank_accounts.py
@@ -2,7 +2,7 @@ class User:
     def __init__(self, name, email):
         self.name = name
         self.email = email
-        self.account_balance = 0
+        self.account = BankAccount(int_rate=0.02, balance=0)
 
     # instance methods
     def greeting(self):
@@ -10,30 +10,29 @@ class User:
         return self
 
     def make_deposit(self, amount):
-        self.account_balance += amount
+        self.account.deposit(amount)
         print(f"{self.name}'s account balance is now {self.account_balance} dollars.")
         return self
 
     def make_withdrawal(self, amount):
-        self.account_balance -= amount
+        self.account.withdraw(amount)
         print(f"{self.name}'s account balance is now {self.account_balance} dollars.")
         return self
 
     def display_user_balance(self):
-        print(f"{self.name}'s account balance is currently {self.account_balance} dollars.")
+        self.account.display_account_info()
         return self
 
     def transfer_money(self, other_user, amount):
-        self.account_balance -= amount
-        other_user.account_balance += amount
-        print(f"{other_user.name}'s account balance is now {other_user.account_balance} dollars.")
-        print(f"{self.name}'s account balance is now {self.account_balance} dollars.")
+        self.account.balance -= amount
+        other_user.account.balance += amount
+        print(f"{other_user.name}'s account balance is now {other_user.account.balance} dollars.")
+        print(f"{self.name}'s account balance is now {self.account.balance} dollars.")
         return self
 
 class BankAccount:
     accounts = []
-    def __init__(self, name, int_rate, balance=0):
-        self.name = name
+    def __init__(self, int_rate, balance=0):
         self.int_rate = int_rate
         self.balance = balance
         BankAccount.accounts.append(self)
@@ -47,7 +46,7 @@ class BankAccount:
         print(f"Withdrawal successful. Balance is now {self.balance}.")
         return self
     def display_account_info(self):
-        print(f"{self.name} balance is currently {self.balance}")
+        print(f"Balance is currently {self.balance}")
         return self
     def yield_interest(self):
         if self.balance > 0:
@@ -61,3 +60,12 @@ class BankAccount:
     def print_all_accounts(cls):
         for account in cls.accounts:
             account.display_account_info()
+
+# User instances
+max = User("Max", "max@gmail.com")
+tom = User("Tom", "tommy@yahoo.com")
+
+max.account.deposit(150)
+max.display_user_balance()
+max.account.display_account_info()
+max.transfer_money(tom, 75)
