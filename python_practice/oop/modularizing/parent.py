9cfd2edfb30449a471cab0fecb72b43aa02dcba1
Max Barr <mxbarr@gmail.com>
2022-12-31 15:49:46 -0800

    Practice modularizing files

--- python_practice/oop/modularizing/parent.py
+++ python_practice/oop/modularizing/parent.py
@@ -13,3 +13,10 @@ print(square(4))
 user = User("Emma")
 print(user.name)
 print(user.say_hello())
+
+print(__name__)
+
+if __name__ == "__main__":
+    print("The file is being executed directly")
+else:
+    print("The file is being executed because it is being imported by another file. The file is called: ", __name__)
