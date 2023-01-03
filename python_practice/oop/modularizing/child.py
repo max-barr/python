692a2add0b589075484ea40f44c4988e60dfd913
Max Barr <mxbarr@gmail.com>
2023-01-02 16:27:10 -0800

    Practice with overriding and polymorphism

--- python_practice/oop/modularizing/child.py
+++ python_practice/oop/modularizing/child.py
@@ -1,3 +1,16 @@
 import parent
 
-print(locals())
+class Person:
+    def pay_bill(self):
+        raise NotImplementedError
+class Millionaire(Person):
+    def pay_bill(self):
+        print("Keep the change you filthy animal.")
+class GradStudent(Person):
+    def pay_bill(self):
+        print("Can I owe you?")
+
+chad = GradStudent()
+chad.pay_bill()
+
+# print(locals())
