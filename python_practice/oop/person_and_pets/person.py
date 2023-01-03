ac0ed2d139c39819526027f3c770e9890cbae6cd
Max Barr <mxbarr@gmail.com>
2023-01-02 16:44:56 -0800

    Practice with OOP and class association

---
+++ python_practice/oop/person_and_pets/person.py
@@ -0,0 +1,16 @@
+import pet
+
+class Person:
+    def __init__(self, first_name, last_name, pet, treats, pet_food):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.pet = pet
+        self.treats = treats
+        self.pet_food = pet_food
+    # class methods
+    def walk(self):
+        pass
+    def feed(self):
+        pass
+    def bathe(self):
+        
---
+++ python_practice/oop/person_and_pets/pet.py
@@ -0,0 +1,19 @@
+import person
+
+class Pet:
+    def __init__(self, name, species, tricks, health, energy):
+        self.name = name
+        self.species = species
+        self.tricks = tricks
+        self.health = health
+        self.energy = energy
+    # class methods
+    def sleep(self):
+        pass
+    def eat(self):
+        pass
+    def play(self):
+        pass
+    def noise(self):
+        pass
+    
