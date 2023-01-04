8b8720a4d27a667209551b858b2b61d9286ea4d7
Max Barr <mxbarr@gmail.com>
2023-01-03 16:57:32 -0800

    Practice creating, associating classes

--- python_practice/oop/store_and_products/product.py
+++ python_practice/oop/store_and_products/product.py
@@ -9,5 +9,9 @@ class Product:
             self.price += (self.price * percent_change)
         else:
             self.price -= (self.price * percent_change)
+        return self
+    def print_info(self):
+        print(f"Name: {self.name}\n Category: {self.category}\n Price: {self.price}")
+        return self
 
 
--- python_practice/oop/store_and_products/store.py
+++ python_practice/oop/store_and_products/store.py
@@ -3,4 +3,7 @@ class Store:
         self.name = name
         self.products = []
     # Class methods
-    
+    def add_product(self, new_product):
+        self.products.append(new_product)
+        print(f"{new_product} has been added to the store.")
+        return self
