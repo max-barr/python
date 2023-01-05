15c485e7588209c5b29df2673e7bd86327ec1adb
Max Barr <mxbarr@gmail.com>
2023-01-04 16:43:04 -0800

    Practice creating classes, chaining methods

---
+++ python_practice/oop/MathClass.py
@@ -0,0 +1,19 @@
+class MathClass:
+    def __init__(self):
+        self.result = 0
+    def add(self, num, *nums):
+        self.result += num
+        for i in nums:
+            self.result += i
+        return self
+    def subtract(self, num, *nums):
+        self.result -= num
+        for i in nums:
+            self.result -= i
+        return self
+
+mc = MathClass()
+
+x = mc.add(2).add(2,5,1).subtract(3,2).result
+
+print(x)
