ea4fa414529fe4f1b63675b1a66f4e854656367d
Max Barr <mxbarr@gmail.com>
2023-01-17 14:03:53 -0800

    Practice writing unit tests in Python

--- unit_testing/test_odd_nums.py
+++ unit_testing/test_odd_nums.py
@@ -1,4 +1,23 @@
 # Import the testing framework
 import unittest
 
+# This is the unit we are running tests on 
+def odd_num(num):
+    if num % 2 != 0:
+        return True
+    else:
+        return False
+    
+# The unit test is initialized by creating a class that inherits from unittest.TestCase
+class isOddTests(unittest.TestCase):
+    # Each method is its own test
+    def testOne(self):
+        self.assertEqual(odd_num(3), True)
+    def testTwo(self):
+        self.assertTrue(odd_num(55))
+    def testThree(self):
+        self.assertEqual(odd_num(66), False)
 
+if __name__ == '__main__':
+    # This runs our tests
+    unittest.main()
