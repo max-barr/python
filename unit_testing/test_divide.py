28c5bc9f2cb097bf1e05e5b2b80341dfe2834433
Max Barr <mxbarr@gmail.com>
2023-01-19 16:39:41 -0800

    Practicing writing and running unit tests

--- unit_testing/test_divide.py
+++ unit_testing/test_divide.py
@@ -5,7 +5,14 @@ def division(x,y):
 
 # The unit test is initialized by creating a class that inherits from unittest.TestCase
 class divisionTests(unittest.TestCase):
-    
+    def testOne(self):
+        self.assertEqual(division(4,2), 2)
+    def testTwo(self):
+        self.assertEqual(division(100,25), 4)
+    def testThree(self):
+        self.assertNotEqual(division(6,3), 3)
+    def testFour(self):
+        self.assertEqual(division(50,2), 25)
 
 
 
