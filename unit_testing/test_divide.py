83a83fc2d4291773644108b156a041b81f8efb37
Max Barr <mxbarr@gmail.com>
2023-01-19 16:37:42 -0800

    Practice writing and running unit tests in Python

---
+++ unit_testing/test_divide.py
@@ -0,0 +1,14 @@
+import unittest
+
+def division(x,y):
+    return x / y
+
+# The unit test is initialized by creating a class that inherits from unittest.TestCase
+class divisionTests(unittest.TestCase):
+    
+
+
+
+# This runs our tests
+if __name__ == '__main__':
+    unittest.main()
