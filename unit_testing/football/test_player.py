5b156bdd8143e9fadd0f6a90bab3a76068f5a02d
Max Barr <mxbarr@gmail.com>
2023-01-23 15:07:18 -0800

    Python unit test practice

--- unit_testing/football/test_player.py
+++ unit_testing/football/test_player.py
@@ -7,6 +7,7 @@ class TestPlayer(unittest.TestCase):
         print('Setup')
         self.player_1 = Player('Joe', 'Montana', 'Quarterback', 'San Francisco 49ers', 16)
         self.player_2 = Player('Justin', 'Jefferson', 'Wide Receiver', 'Minnesota Vikings', 84)
+        self.player_3 = Player('Patrick', 'Mahomes', 'Quarterback', 'Kansas City Chiefs', 15)
 
     def tearDown(self):
         print('Teardown')
@@ -14,6 +15,7 @@ class TestPlayer(unittest.TestCase):
     def test_player_card(self):
         self.assertEqual(self.player_1.player_card, 'Joe Montana, Quarterback for the San Francisco 49ers, number 16')
         self.assertEqual(self.player_2.player_card, 'Justin Jefferson, Wide Receiver for the Minnesota Vikings, number 84')
+        self.assertEqual(self.player_3.player_card, 'Patrick Mahomes, Quarterback for the Kansas City Chiefs, number 15')
 
 if __name__ == "__main__":
     unittest.main()
