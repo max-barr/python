30e6fef6a087c04678c7db238aecb1de5b1ebb69
Max Barr <mxbarr@gmail.com>
2023-01-24 15:50:09 -0800

    Unit test practice in python

---
+++ unit_testing/basketball/Player.py
@@ -0,0 +1,12 @@
+class Player:
+
+    def __init__(self, first_name, last_name, position, team, number):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.position = position
+        self.team = team
+        self.number = number
+
+    @property
+    def player_card(self):
+        return '{} {}, number {}, {} for the {}'.format(self.first_name, self.last_name, self.number, self.position, self.team)
