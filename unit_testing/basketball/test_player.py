b33fcc9bcb6fe98298daa21b2e3bc63177de6566
Max Barr <mxbarr@gmail.com>
2023-01-25 16:00:13 -0800

    Python unit test practice

--- unit_testing/basketball/test_player.py
+++ unit_testing/basketball/test_player.py
@@ -9,13 +9,13 @@ class TestPlayer(unittest.TestCase):
         self.player_2 = Player('LeBron', 'James', 'Forward', 'Los Angeles Lakers', 6)
         self.player_3 = Player('Joel', 'Embiid', 'Center', 'Philadelphia 76ers', 21)
 
-    def tearDown(self):
-        print("Running Teardown")
-
     def test_player_card(self):
         self.assertEqual(self.player_1.player_card, 'Damian Lillard, number 0, Point Guard for the Portland Trail Blazers')
         self.assertEqual(self.player_2.player_card, 'LeBron James, number 6, Forward for the Los Angeles Lakers')
         self.assertEqual(self.player_3.player_card, 'Joel Embiid, number 21, Center for the Philadelphia 76ers')
+        
+    def tearDown(self):
+        print("Running Teardown")
 
 if __name__ == "__main__":
     unittest.main()
--- unit_testing/basketball/test_score.py
+++ unit_testing/basketball/test_score.py
@@ -7,16 +7,19 @@ class TestScore(unittest.TestCase):
         self.assertEqual(score.field_goal(24), 26)
         self.assertEqual(score.field_goal(2), 4)
         self.assertEqual(score.field_goal(0), 2)
+        self.assertIs(score.field_goal(39), 41)
 
     def test_free_throw(self):
         self.assertEqual(score.free_throw(1), 2)
         self.assertEqual(score.free_throw(99), 100)
         self.assertEqual(score.free_throw(5), 6)
+        self.assertNotEqual(score.free_throw(19), 21)
 
     def test_three_pointer(self):
         self.assertEqual(score.three_pointer(1), 4)
         self.assertEqual(score.three_pointer(25), 28)
         self.assertEqual(score.three_pointer(15), 18)
+        self.assertIs(score.three_pointer(3), 6)
 
 # This runs our test
 if __name__ == "__main__":
