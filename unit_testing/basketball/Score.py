6eaa7c5fd3f300aaf23893e0f3b79d84c8f21009
Max Barr <mxbarr@gmail.com>
2023-01-26 15:38:28 -0800

    Added method

--- unit_testing/basketball/Score.py
+++ unit_testing/basketball/Score.py
@@ -1,3 +1,5 @@
+import random
+
 def field_goal(score):
     if score < 0:
         raise ValueError("Score cannot be negative")
@@ -16,3 +18,10 @@ def three_pointer(score):
     score += 3
     return score
 
+def jump_ball(team_1, team_2):
+    if random.randint(1,2) == 1:
+        print("The {} won the ball".format(team_1))
+    else:
+        print("The {} won the ball".format(team_2))
+
+jump_ball("Portland Trail Blazers", "Los Angeles Lakers")
