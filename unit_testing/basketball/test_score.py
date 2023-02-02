fe98fcb277f3dd5e10f2153bdfa41ea5d1cae9e1
Max Barr <mxbarr@gmail.com>
2023-02-02 13:25:04 -0800

    Unit test practice in python

--- unit_testing/basketball/test_score.py
+++ unit_testing/basketball/test_score.py
@@ -7,18 +7,21 @@ class TestScore(unittest.TestCase):
         self.assertEqual(score.field_goal(24), 26)
         self.assertEqual(score.field_goal(2), 4)
         self.assertEqual(score.field_goal(0), 2)
+        self.assertEqual(score.field_goal(20), 22)
         self.assertIs(score.field_goal(39), 41)
 
     def test_free_throw(self):
         self.assertEqual(score.free_throw(1), 2)
         self.assertEqual(score.free_throw(99), 100)
         self.assertEqual(score.free_throw(5), 6)
+        self.assertEqual(score.free_throw(55), 56)
         self.assertNotEqual(score.free_throw(19), 21)
 
     def test_three_pointer(self):
         self.assertEqual(score.three_pointer(1), 4)
         self.assertEqual(score.three_pointer(25), 28)
         self.assertEqual(score.three_pointer(15), 18)
+        self.assertEqual(score.three_pointer(115), 118)
         self.assertIs(score.three_pointer(3), 6)
 
 # This runs our test
