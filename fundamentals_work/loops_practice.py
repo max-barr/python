1663cfea30e65be73bb5f581e4f804ebb648b05b
Max Barr <mxbarr@gmail.com>
2023-04-18 12:36:11 -0700

    Practice with loops and conditionals

--- fundamentals_work/loops_practice.py
+++ fundamentals_work/loops_practice.py
@@ -32,3 +32,10 @@ for i in range(2018,0,-4):
 
 # Flexible Counter - Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult. For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
 
+def flexible_counter(lowNum, highNum, mult):
+    for i in range(lowNum, highNum + 1):
+        if i % mult == 0:
+            print(i)
+
+flexible_counter(2,9,3)
+flexible_counter(1,17,2)
