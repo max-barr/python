08e6651370a95e9ac33b6b136916be0d383eb8ff
Max Barr <mxbarr@gmail.com>
2023-04-19 11:06:33 -0700

    Practice with nested dictionaries

--- fundamentals_work/dictionaries.py
+++ fundamentals_work/dictionaries.py
@@ -19,3 +19,25 @@ print("My name is " + person["first"] + " " + person["last"])
 
 value_removed = person.pop("is_organ_donor")
 print(person)
+
+# NESTED DICTIONARIES
+
+# List of dictionaries
+users = [
+    {"first": "Ada", "last": "Lovelace"}, # index 0
+    {"first": "Alan", "last": "Turing"}, # index 1
+    {"first": "Eric", "last": "Idle"} # index 2
+]
+# Dictionary of lists
+resume_data = {
+    #        	     0           1           2
+    "skills": ["front-end", "back-end", "database"],
+    #                0           1
+    "languages": ["Python", "JavaScript"],
+    #                0              1
+    "hobbies":["rock climbing", "knitting"]
+}
+
+print(users[0]["last"])
+print(resume_data["skills"][1])
+print(users[2]["first"])
