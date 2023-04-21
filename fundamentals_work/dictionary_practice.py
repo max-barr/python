b4397d83793957b2e56ccafe8f14df2c117580e7
Max Barr <mxbarr@gmail.com>
2023-04-21 08:57:47 -0700

    Practice iterating through dictionaries

--- fundamentals_work/dictionary_practice.py
+++ fundamentals_work/dictionary_practice.py
@@ -39,3 +39,36 @@ def iterateDictionary(list):
             print(key + ": " + val)
 
 iterateDictionary(students)
+
+# --------------------
+
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+def iterateDictionary2(key_name, list):
+    # i is now a dictionary
+    for i in list:
+        print(i[key_name])
+
+iterateDictionary2('last_name', students)
+
+# ------------------
+
+schools = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+# print(schools['locations'][1])
+
+def print_info(dict):
+    for key in dict:
+        print(f"{len(dict[key])} {key}")
+        for i in dict[key]:
+            print(i)
+
+print_info(schools)
