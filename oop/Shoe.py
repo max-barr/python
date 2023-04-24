d1526e5f9ad146ea131fec849d468c110c912719
Max Barr <mxbarr@gmail.com>
2023-04-24 10:42:46 -0700

    Added class methods

--- oop/Shoe.py
+++ oop/Shoe.py
@@ -13,10 +13,32 @@ class Shoe:
         else: 
             print(f"Sorry, the {self.brand} {self.name} is currently out of stock")
 
+    def on_sale(self, percent_off):
+        self.price *= (1 - percent_off)
 
+    def add_tax(self, tax):
+        self.price *= (1 + tax)
 
+    def price_cut(self, money):
+        if money > self.price:
+            print("Sorry, we can't cut the price of this shoe.")
+            return False
+        else: 
+            self.price -= money
+
+    def hike_price(self, money):
+        self.price += money
+
+# Instances
 dress_shoe = Shoe("Cole Haan", "Work Shoe", "Dress", 89.99, True)
 bball_shoe = Shoe("Nike", "KD 15", "Basketball", 150, False)
+
+
+bball_shoe.add_tax(.07)
+bball_shoe.price_cut(30)
+bball_shoe.hike_price(60)
+bball_shoe.on_sale(0.2)
+print(bball_shoe.price)
 
 dress_shoe.stock_check()
 bball_shoe.stock_check()
