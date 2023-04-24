70b6e15be8a226f58f4ba09eff9e1f9d65d40eae
Max Barr <mxbarr@gmail.com>
2023-04-24 09:36:47 -0700

    Practice creating classes

---
+++ oop/shoe_class.py
@@ -0,0 +1,10 @@
+class Shoe:
+    def __init__(self, brand, activity, price, stock_status):
+        self.brand = brand
+        self.activity = activity
+        self.price = price
+        self.stock_status = stock_status
+
+    
+
+
