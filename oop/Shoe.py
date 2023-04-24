1be608030ffb9e764c19556a63e5e405755a6692
Max Barr <mxbarr@gmail.com>
2023-04-24 10:16:36 -0700

    Practice creating classes, instances

--- oop/Shoe.py
+++ oop/Shoe.py
@@ -1,10 +1,14 @@
 class Shoe:
-    def __init__(self, brand, activity, price, stock_status):
+    def __init__(self, brand, activity, price, in_stock):
         self.brand = brand
         self.activity = activity
         self.price = price
-        self.stock_status = stock_status
+        self.in_stock = in_stock
 
-    
+
+
+dress_shoe = Shoe("Cole Haan", "Dress", 89.99, True)
+bball_shoe = Shoe("Nike", "Basketball", 150, True)
+
 
 
