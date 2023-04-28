d87513352573a55c46563e0e03cabcfe7e345aac
Max Barr <mxbarr@gmail.com>
2023-04-28 09:49:47 -0700

    Creating classes

--- oop/user3.py
+++ oop/user3.py
@@ -11,6 +11,18 @@ class User:
     def display_info(self):
         print(f"{self.first_name}\n{self.last_name}\n{self.email}\n{self.age}\nRewards member: {self.is_rewards_member}\nGold Card Points: {self.gold_card_points}")
 
+    def enroll(self):
+        if self.is_rewards_member == True:
+            print("User is already a member")
+            return False
+        else:
+            self.is_rewards_member = True
+            self.gold_card_points = 200
+
+    def spend_points(self, amount):
+        self.gold_card_points -= amount
+        print(f"{self.first_name} {self.last_name} has successfully spent {amount} gold ")
+
 
 
 john = User("John", "Johnson", "john@yahoo.com", 34)
