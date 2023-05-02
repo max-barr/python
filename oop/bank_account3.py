a09618cb8cd6f4b07ca64c8b2e2e827f09bde329
Max Barr <mxbarr@gmail.com>
2023-05-02 13:58:32 -0700

    Created new class, methods, attributes

---
+++ oop/bank_account3.py
@@ -0,0 +1,15 @@
+class BankAccount:
+    # Constructor
+    def __init__(self, int_rate = 0.01, balance = 0):
+        pass
+
+    # Class methods
+    def deposit(self, amount):
+        pass
+    def withdraw(self, amount):
+        pass
+    def display_account_info(self):
+        pass
+    def yield_interest(self):
+        pass
+    
