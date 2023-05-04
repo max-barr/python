608702f269c22d1cc42133f691de2a15422b9673
Max Barr <mxbarr@gmail.com>
2023-05-04 13:59:35 -0700

    Associated classes

--- oop/bank_account3.py
+++ oop/bank_account3.py
@@ -39,6 +39,39 @@ class BankAccount:
         for account in cls.all_accounts:
             account.display_account_info()
 
+class User:
+    def __init__(self, first_name, last_name, email, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.age = age
+        self.is_rewards_member = False
+        self.gold_card_points = 0
+        self.account = BankAccount()
+
+    # METHODS
+    def display_info(self):
+        print(f"{self.first_name}\n{self.last_name}\n{self.email}\n{self.age}\nRewards member: {self.is_rewards_member}\nGold Card Points: {self.gold_card_points}")
+        return self
+
+    def enroll(self):
+        if self.is_rewards_member == True:
+            print("User is already a member")
+            return False
+        else:
+            self.is_rewards_member = True
+            self.gold_card_points = 200
+            return self
+
+    def spend_points(self, amount):
+        if self.gold_card_points - amount > 0:
+            self.gold_card_points -= amount
+            print(f"{self.first_name} {self.last_name} has successfully spent {amount} gold card points.")
+            return self
+        else:
+            print(f"{self.first_name} {self.last_name} does not have enough points.")
+            return self
+
 # Class instances
 account1 = BankAccount()
 account2 = BankAccount()
@@ -46,3 +79,7 @@ account2 = BankAccount()
 account1.deposit(100).deposit(25).deposit(41).withdraw(11).yield_interest().display_account_info()
 
 BankAccount.display_all_accounts()
+
+john = User("John", "Johnson", "john@yahoo.com", 34)
+john.display_info()
+print(john.account.balance)
